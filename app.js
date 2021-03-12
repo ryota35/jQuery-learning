46ea48a14c1c9e5cfc0400c7484b758a77134dac
ryota35 <ryota35.1025@gmail.com>
2021-03-12 22:56:58 +0900

    [test]

---
+++ app.js
@@ -0,0 +1,52 @@
+// $(function () {
+//   $("body").html("<h1>Hello jQuery!!</h1>");
+// });
+
+// $(function () {
+//   $(".box1").css({
+//     "background-color": "#0000FF",
+//     "height": "100px"
+//   });
+// });
+
+// $(function () {
+//   $('.box1').slideDown(function () {
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'height': '100px',
+//     })
+//       .slideUp();
+//   });
+// });
+
+// $(function () {
+//   $('.box1').mouseover(function () {
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function () {
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+// $(function () {
+//   $('.box1').on("click", function () {
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function () {
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
+
+
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $(this).slideUp();
+//   });
+// });
+
+
+$(function () {
+  $('button').on('click', function () {
+    $('ul').children().css({ 'color': 'red' });
+  })
+});
